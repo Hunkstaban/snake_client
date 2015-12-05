686a28a9568760ab47769ad672845a3b952c2bb7
Peter Torgersen <peter.torgersen@gmail.com>
2015-12-05 15:37:45 +0100

    Lavet API og ServerConnection klasse, så der kan kommunikeres og skabes forbindelse med og til serveren.

---
+++ src/SDK/API.java
@@ -0,0 +1,92 @@
+package SDK;
+
+import DTO.Game;
+import DTO.Score;
+import DTO.User;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import org.codehaus.jettison.json.JSONException;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * Created by Peter on 03-12-2015.
+ */
+
+public class API {
+
+    ServerConnection connection = new ServerConnection();
+
+    public ArrayList<User> getAllUsers() {
+
+        ArrayList<User> users = new Gson().fromJson(connection.get("users"), new TypeToken<ArrayList<User>>(){}.getType());
+        return users;
+    }
+
+    public ArrayList<Game> getPendingGames(int userId) {
+        ArrayList<Game> games = new Gson().fromJson(connection.get("games/opponent/" + userId), new TypeToken<ArrayList<Game>>(){}.getType());
+        return games;
+    }
+
+    public String createGame(Game game) {
+        String serverMessage = connection.post(new Gson().toJson(game), "games");
+        return parseMessage(serverMessage);
+    }
+
+    public String joinGame(Game game) {
+        String serverMessage = connection.put(new Gson().toJson(game),"games/join");
+        return parseMessage(serverMessage);
+    }
+
+    public String login(User currentUser) {
+        String serverMessage = connection.post(new Gson().toJson(currentUser), "login");
+        int id = getUserID(serverMessage);
+        currentUser.setId(id);
+        return parseMessage(serverMessage);
+
+    }
+
+    //creating HashMap to get values from server
+    public String parseMessage(String serverMessage) {
+        HashMap<String, String> messageMap = new Gson().fromJson(serverMessage, HashMap.class);
+        return messageMap.get("message");
+    }
+
+    public int getUserID (String serverMessage) {
+        JSONParser jsonParser = new JSONParser();
+        try {
+            Object object = jsonParser.parse(serverMessage);
+            JSONObject jsonObject = (JSONObject)object;
+            return (int)(long)jsonObject.get("userid");
+        } catch (ParseException | NullPointerException e) {
+            e.printStackTrace();
+        }
+        return -1;
+    }
+
+
+    public Game startGame(Game game) {
+        String serverMessage = connection.put(new Gson().toJson(game),"games/start");
+        return new Gson().fromJson(serverMessage, Game.class);
+    }
+
+    public String deleteGame(int gameID) {
+        String serverMessage = connection.delete("games/"+ gameID);
+        return parseMessage(serverMessage);
+    }
+
+    public ArrayList<Game> getAllActiveGames(int userId) {
+        ArrayList<Game> games = new Gson().fromJson(connection.get("games/pending/" + userId), new TypeToken<ArrayList<Game>>(){}.getType());
+        return games;
+    }
+
+    //making an ArrayList with scores
+    public ArrayList<Score> getHighscores() {
+       return new Gson().fromJson(connection.get("highscores"), new TypeToken<ArrayList<Score>>(){}.getType());
+    }
+
+}
---
+++ src/SDK/ServerConnection.java
@@ -0,0 +1,90 @@
+package SDK;
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
+/**
+ * Created by Peter on 03-12-2015.
+ */
+public class ServerConnection {
+
+
+    public ServerConnection(){
+        this.hostAddress="http://localhost";
+        this.port=8888;
+        }
+
+    private String hostAddress;
+    private int port;
+
+    public void setHostAddress(String hostAddress) {
+        this.hostAddress = hostAddress;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    public String getHostAddress() {
+        return hostAddress;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public String get(String path){
+
+        Client client = Client.create();
+
+        WebResource webResource = client.resource(getHostAddress() + ":" + getPort() + "/api/" + path);
+        ClientResponse response = webResource.type("application/json").get(ClientResponse.class);
+
+        String output = response.getEntity(String.class);
+        System.out.println(output);
+
+        return output;
+
+    }
+
+    public String delete(String path){
+
+        Client client = Client.create();
+
+        WebResource webResource = client.resource(getHostAddress() + ":" + getPort() + "/api/" + path);
+        ClientResponse response = webResource.type("application/json").delete(ClientResponse.class);
+
+        String output = response.getEntity(String.class);
+        System.out.println(output);
+
+        return output;
+
+    }
+
+    public String post(String json, String path){
+
+        Client client = Client.create();
+
+        WebResource webResource = client.resource(getHostAddress() + ":" + getPort() + "/api/" + path);
+        ClientResponse response = webResource.type("application/json").post(ClientResponse.class, json);
+
+        String output = response.getEntity(String.class);
+        System.out.println(output);
+
+        return output;
+
+    }
+
+    public String put(String json, String path){
+
+        Client client = Client.create();
+
+        WebResource webResource = client.resource(getHostAddress() + ":" + getPort() + "/api/" + path);
+        ClientResponse response = webResource.type("application/json").put(ClientResponse.class, json);
+
+        String output = response.getEntity(String.class);
+        System.out.println(output);
+
+        return output;
+
+    }
+}
